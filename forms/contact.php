d955c987222ecca86d2d5d0f1dfe714107a3cbd5
fortheloveofcreation <swathired123@gmail.com>
2025-06-25 10:36:52 -0400

    25th-jun update

--- forms/contact.php
+++ forms/contact.php
@@ -12,7 +12,7 @@
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
   } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
+    die( 'Please email swathired123@outlook.com directly as the server is down');
   }
 
   $contact = new PHP_Email_Form;
